180a7a2742ccf2d715325b9977ce5a705598b827
Nishadi De Silva <nds@sanger.ac.uk>
2015-02-06 15:09:07 +0000

    Do not delete files before bsub finishes

--- bio_assembly_refinement/reassembly.py
+++ bio_assembly_refinement/reassembly.py
@@ -75,8 +75,8 @@ class Reassembly:
 		if os.path.exists(default_results_file):
 			shutil.move(default_results_file, self.output_file)
 		
-		if (not self.debug) and os.path.isdir(self.output_directory):
-			shutil.rmtree(self.output_directory)
+#		if (not self.debug) and os.path.isdir(self.output_directory):
+#			shutil.rmtree(self.output_directory)
 			
 		os.chdir(original_dir)
 		

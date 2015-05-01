4f2bf9ccadd998a4990dc1e4bc950400fc4d0bb5
nds <nds@sanger.ac.uk>
2015-05-01 15:11:13 +0100

    new tests

--- bio_assembly_refinement/tests/contig_break_finder_test.py
+++ bio_assembly_refinement/tests/contig_break_finder_test.py
@@ -2,6 +2,8 @@ import unittest
 import filecmp
 import os
 from bio_assembly_refinement import contig_break_finder 
+from pyfastaq import tasks
+
 
 modules_dir = os.path.dirname(os.path.abspath(contig_break_finder.__file__))
 data_dir = os.path.join(modules_dir, 'tests', 'data')
@@ -10,50 +12,45 @@ class TestContigBreakFinder(unittest.TestCase):
 	def test_finding_dnaA(self):
 		
 		tests = [
-			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "Providencia_rustigianii.fa"), 
-							      											 gene_file = os.path.join(data_dir, "all_dnaA.fa"), 
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKING_input_0.fa"), 
+							      											 gene_file = os.path.join(data_dir, "test_dnaA_1.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = False,	
+							      											 skip = os.path.join(data_dir, "BREAKING_skip_ids_0.txt")
+							      											), 
+			'BREAKING_input_0.fa' ], #skip 1, dnaA normal - nothing should change
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKING_input_0.fa"), 
+							      											 gene_file = os.path.join(data_dir, "test_dnaA_1.fa"), 
+							      											 choose_random_gene=False, 	
+							      											 rename = False,
+							      											 skip = os.path.join(data_dir, "BREAKING_skip_ids_0_all.txt")
+							      											), 
+			'BREAKING_input_0.fa' ], #skip all - - nothing should change
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKING_input_0.fa"), 
+							      											 gene_file = os.path.join(data_dir, "test_dnaA_1.fa"),
+							      											 rename = False, 
 							      											 choose_random_gene=False, 	
 							      											), 
-			'BREAKING_Providencia_rustigianni.fa' ],
- 			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "Providencia_rustigianii.fa"),
-                                                                gene_file = os.path.join(data_dir, "all_dnaA.fa"),
-							      								skip = os.path.join(data_dir, "BREAKING_skip_ids.txt"),	
-							      								choose_random_gene=False, 	
-                                                              ),
-            'BREAKING_Providencia_rustigianni_2.fa' ],            
-             [contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKING_input_3.fa"),
-                                                                gene_file = os.path.join(data_dir, "all_dnaA.fa"),
-							      								skip = os.path.join(data_dir, "BREAKING_skip_ids_3.txt"),
-							      								rename = False,	
-							      								choose_random_gene=True, 	
-                                                              ),
-            'BREAKING_Providencia_rustigianni_3.fa' ],
- 
+			'BREAKING_output_0_all.fa' ], #skip none - test1 contig should be circularised
+			
 			]
 				
 		for t in tests:
 			t[0].run()
 			self.assertTrue(os.path.isfile(t[0].output_file))
 			self.assertTrue(os.path.isfile(t[0].summary_file))
-#			self.assertTrue(filecmp.cmp(t[0].output_file, os.path.join(data_dir, t[1]) , shallow=False)) 
+			# Read expected output file and compare sequences
+			expected_contigs = {}
+			tasks.file_to_dict(os.path.join(data_dir, t[1]), expected_contigs)
+			for id in expected_contigs.keys():
+# 				print(id + "\n")
+# 				print("Expected: " + expected_contigs[id].seq)
+# 				print("Got : " + t[0].contigs[id].seq)
+				self.assertTrue(expected_contigs[id] == t[0].contigs[id])
 			os.remove(t[0].output_file)
 			os.remove(t[0].summary_file)	
 
-	def test_skipping_all(self):
-			
-		breaker = contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "Providencia_rustigianii.fa"),
-                                                            gene_file = os.path.join(data_dir, "all_dnaA.fa"),
-							      							skip = os.path.join(data_dir, "BREAKING_skip_ids_2.txt"),	
-							      							choose_random_gene=False, 
-							      							rename = False,	
-                                                            )
-		breaker.run()
-		
-		expected_summary_file = os.path.join(data_dir, "BREAKING_summary_file.txt")            
-		self.assertTrue(os.path.isfile(breaker.output_file))
-		self.assertTrue(os.path.isfile(breaker.summary_file))
-# 		self.assertTrue(filecmp.cmp(breaker.summary_file, expected_summary_file , shallow=False)) 
-		os.remove(breaker.summary_file)     
-		os.remove(breaker.output_file)       
-		
+# 	def test_skipping_all(self):
+# 		''' Run prodigal in various situations '''
+
 		

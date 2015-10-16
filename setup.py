344c07f2b5023a508391dd69057de1dd2ffb2bda
martinghunt <martin.g.hunt@gmail.com>
2015-10-16 16:04:47 +0100

    nose required by tests, not install

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     url='https://github.com/nds/bio_assembly_refinement',
     scripts=glob.glob('scripts/*'),
     test_suite='nose.collector',
-    install_requires=['nose >= 1.3', 'pyfastaq >= 3.10.0'],
+    tests_require=['nose >= 1.3'],
+    install_requires=['pyfastaq >= 3.10.0'],
     license='GPLv3',
 )

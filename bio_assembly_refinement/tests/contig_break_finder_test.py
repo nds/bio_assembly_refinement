5c7d8b961ea6f89f83543ce56a4d032a8811a8f1
nds <nds@sanger.ac.uk>
2015-10-27 14:25:45 +0000

    test new dnaa search functionality

--- bio_assembly_refinement/tests/contig_break_finder_test.py
+++ bio_assembly_refinement/tests/contig_break_finder_test.py
@@ -9,48 +9,125 @@ modules_dir = os.path.dirname(os.path.abspath(contig_break_finder.__file__))
 data_dir = os.path.join(modules_dir, 'tests', 'data')
 
 class TestContigBreakFinder(unittest.TestCase):
-	def test_finding_dnaA(self):
+	def test_finding_dnaA_in_various_positions(self):
 		
 		tests = [
-			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKING_input_0.fa"), 
-							      											 gene_file = os.path.join(data_dir, "test_dnaA_1.fa"), 
+			#dnaa at start - return identical sequence
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_dnaa_at_start.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
 							      											 choose_random_gene=False, 
 							      											 rename = False,	
-							      											 skip = os.path.join(data_dir, "BREAKING_skip_ids_0.txt")
 							      											), 
-			'BREAKING_input_0.fa' ], #skip 1, dnaA normal - nothing should change
-			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKING_input_0.fa"), 
-							      											 gene_file = os.path.join(data_dir, "test_dnaA_1.fa"), 
-							      											 choose_random_gene=False, 	
-							      											 rename = False,
-							      											 skip = os.path.join(data_dir, "BREAKING_skip_ids_0_all.txt")
-							      											), 
-			'BREAKING_input_0.fa' ], #skip all - - nothing should change
-			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKING_input_0.fa"), 
-							      											 gene_file = os.path.join(data_dir, "test_dnaA_1.fa"),
-							      											 rename = False, 
-							      											 choose_random_gene=False, 	
-							      											), 
-			'BREAKING_output_0_all.fa' ], #skip none - test1 contig should be circularised
+													'BREAKFINDER_output_dnaa_at_start.fa' ], 
 			
-			]
+			# dnaa in the middle
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_dnaa_in_middle.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = False,	
+							      											), 
+													'BREAKFINDER_output_dnaa_in_middle.fa' ], 
+			
+			# dnaa at the end
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_dnaa_at_end.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = False,	
+							      											), 
+													'BREAKFINDER_output_dnaa_at_end.fa' ], 
+													
+			# dnaa split across start and end
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_dnaa_split.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = False,	
+							      											), 
+													'BREAKFINDER_output_dnaa_split.fa' ], 
+			# dnaa in middle of contig  but revcom
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_dnaa_in_middle_revcom.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = False,	
+							      											), 
+													'BREAKFINDER_output_dnaa_in_middle.fa' ], 											
+			# dnaa split across start and end but revcom
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_dnaa_split_revcom.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = False,	
+							      											), 
+													'BREAKFINDER_output_dnaa_split_revcom.fa' ], 
+			# no dnaa 
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_no_dnaa.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = False,	
+							      											), 
+													'BREAKFINDER_input_no_dnaa.fa' ], #do not change the contig
+			# best dnaa hit not first 
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_multiple_dnaa.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_multiple_dnaA.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = False,	
+							      											), 
+													'BREAKFINDER_output_multiple_dnaa.fa' ], 			
+
+ 			]
 				
 		for t in tests:
 			t[0].run()
 			self.assertTrue(os.path.isfile(t[0].output_file))
 			self.assertTrue(os.path.isfile(t[0].summary_file))
-			# Read expected output file and compare sequences
 			expected_contigs = {}
 			tasks.file_to_dict(os.path.join(data_dir, t[1]), expected_contigs)
 			for id in expected_contigs.keys():
-# 				print(id + "\n")
-# 				print("Expected: " + expected_contigs[id].seq)
-# 				print("Got : " + t[0].contigs[id].seq)
-				self.assertTrue(expected_contigs[id] == t[0].contigs[id])
+				self.assertTrue(expected_contigs[id].seq == t[0].contigs[id].seq)
 			os.remove(t[0].output_file)
 			os.remove(t[0].summary_file)	
 
-# 	def test_skipping_all(self):
-# 		''' Run prodigal in various situations '''
 
-		
+
+	def test_options(self):	
+		tests_for_options = [
+			#rename genes
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_dnaa_at_start.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = True,	
+							      											), 
+													'BREAKFINDER_output_dnaa_at_start.fa' ], 	
+			# no dnaa, but use prodigal	
+# 			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_no_dnaa.fa"), 
+# 							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
+# 							      											 choose_random_gene=True, 
+# 							      											 rename = False,	
+# 							      											), 
+# 													'BREAKFINDER_input_no_dnaa.fa' ], #do not change the contig										
+			# skip one contig
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_multiple_contigs.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = False,	
+							      											 skip = os.path.join(data_dir, "BREAKFINDER_skip_one_id.txt")
+							      											), 
+													'BREAKFINDER_output_skip_contig.fa' ], 				
+			# skip all contigs	
+			[contig_break_finder.ContigBreakFinder(fasta_file = os.path.join(data_dir, "BREAKFINDER_input_multiple_contigs.fa"), 
+							      											 gene_file = os.path.join(data_dir, "BREAKFINDER_test_dnaA.fa"), 
+							      											 choose_random_gene=False, 
+							      											 rename = True,	
+							      											 skip = os.path.join(data_dir, "BREAKFINDER_skip_all.txt")
+							      											), 
+													'BREAKFINDER_input_multiple_contigs.fa' ], 	# do not change anything							
+			]
+			
+		for t in tests_for_options:
+			t[0].run()
+			expected_contigs = {}
+			tasks.file_to_dict(os.path.join(data_dir, t[1]), expected_contigs)
+			for id in expected_contigs.keys():
+				self.assertTrue(expected_contigs[id].seq == t[0].contigs[id].seq)
+			os.remove(t[0].output_file)
+			os.remove(t[0].summary_file)	
+	
+	
